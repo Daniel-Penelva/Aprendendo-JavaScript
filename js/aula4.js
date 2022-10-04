49b02315191682aa8144038fe381b145c9ee2ba5
Daniel Penelva <d4n.andrade@gmail.com>
2022-10-04 03:12:21 -0300

    Alterado arquivo de array

--- js/aula4.js
+++ js/aula4.js
@@ -2,6 +2,12 @@ var array = [1, 2, 3, 4, 5, 6];
 console.log(array);
 console.log(typeof array);  // tipo Object
 
+let carro = ['ferrari', 'mercedes', 'honda']
+document.write("Carros: " + carro)
+
+var codigos = Array(10, 20, 30)
+document.write("Códigos: " + codigos)
+
 /* capturando por indice */
 console.log(array[0]);
 console.log(array[1]);
@@ -29,3 +35,46 @@ array[8] = 9;
 
 console.log(array);
 console.log(array.length);
+
+
+let produtos = ['arroz', 'feijao', 'macarrão']
+var numeros = Array(1, 2, 3, 5, 6)
+
+console.log(produtos)
+console.log(numeros)
+
+//adicionar no final push (empurre)
+produtos.push('batata')
+numeros.push(7)
+
+console.log(produtos)
+console.log(numeros)
+
+
+//remover no final (estourar)
+produtos.pop()
+numeros.pop()
+
+console.log(produtos)
+console.log(numeros)
+
+//adicionar no inicio - unshift 
+produtos.unshift('leite', 'maça')
+console.log(produtos)
+
+//remove do inicio 
+produtos.shift()
+
+//splice(inicio, quantos remover) - emendar
+numeros.splice(1, 2) 
+
+//slice - copiar array slice e fatiar porção
+//posição inicial, quantos copiar
+numeros.slice(0, 2)
+
+//lenght - tamanho do array
+numeros.length
+
+
+// spreed operator...
+let teste = [...produtos, 'Ovo', 'Pera']
